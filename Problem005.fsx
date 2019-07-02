f79b40004705dad21ccb5828df17e9382148fb08
Dmitry <jonmagon@gmail.com>
2019-07-02 17:59:03 +0300

    Code style

--- Problem005.fsx
+++ Problem005.fsx
@@ -2,6 +2,7 @@ let isDividedByAll n = Seq.forall (fun x -> n % x = 0)
 
 let numbers = [|2..20|]
 let max = Array.max(numbers)
+
 Seq.unfold (fun x -> Some(x, x + max)) max
 |> Seq.filter (fun x -> isDividedByAll x numbers)
 |> Seq.head |> printf "%A"
